dab45c6a3bbcfb882b9f836ad0d10c451017f72c
Mohana Misra <iammohanamisra@gmail.com>
2023-06-27 14:28:55 +0530

    implemented checkBoardEmpty()

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let fist = `<img src="images/fist.png" class="fist">`;
+let swords = `<img src="images/swords.png" class="fist">`;
 let boxes = document.getElementsByClassName('box');
 
 let gameboard = document.getElementById('gameboard');
@@ -8,5 +9,31 @@ gameboard.addEventListener('click', function playerMove(event) {
   if (clickedBox.classList.contains('box') && clickedBox.innerHTML === '') {
     clickedBox.innerHTML += fist;
   }
+  aiMove();
 });
 
+function aiMove() {
+  if(checkBoardEmpty()) {
+    let aiBox = boxes[Math.floor(Math.random() * 8)];
+    
+    if(aiBox.innerHTML === "") {
+      aiBox.innerHTML += swords;
+    }
+    else {
+      aiMove();
+  }
+}
+}
+
+function checkBoardEmpty(){
+  let i = 0;
+  while(i < boxes.length) {
+    if(boxes[i].innerHTML === "") {
+      return true;
+    }
+    else {
+      i++;
+    }
+  }
+  return false;
+}
